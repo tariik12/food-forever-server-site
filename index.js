927cf68b2be2de97ac7c8113a316602c9475930c
tarikul-islam <tariikul12@gmail.com>
2023-05-03 01:01:31 +0600

    Data update

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const lotti_cooking = require('./data/lotti_cooking.json')
+const chefs = require('./data/chefs.json')
+
+const recipes = require('./data/recipe.json')
 
 app.use(cors())
 
 app.get('/', (req, res) =>{
     res.send('Food Forever is running')
 });
-app.get('/lotti', (req, res) =>{
-    res.send(lotti_cooking)
+app.get('/chefs/', (req, res) =>{
+    res.send(chefs)
 });
+// app.get('/chefs/:id',(req,res)=>{
+//     const id = parseInt(req.params.id);
+//     console.log(id)
+//     const chef = chefs.find(ch =>ch.id ===id);
+//     res.send(chef)
+// })
+app.get('/recipes', (req, res) =>{
+    res.send(recipes)
+});
+app.get('/chefs/:id', (req, res) =>{
+    const id =parseInt(req.params.id);
+    console.log(id)
+        const chef_recipe = recipes.find(recipe =>recipe.chef_id === id);
+        res.send(chef_recipe)
+})
 
 app.listen(port,() => {
     console.log(`Food Forever is running on port :${port}`)
