aebc03589ce920874db00745956bcc2e89fe3138
tarikul-islam <tariikul12@gmail.com>
2023-05-04 23:26:17 +0600

    data update

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ const port = process.env.PORT || 5000;
 const chefs = require('./data/chefs.json')
 
 const recipes = require('./data/recipe.json')
+const alRecipes = require('./data/allRecipes.json')
 
 app.use(cors())
 
 app.get('/', (req, res) =>{
     res.send('Food Forever is running')
 });
+app.get('/alRecipes', (req, res) =>{
+    console.log(alRecipes)
+    res.send(alRecipes)
+});
+app.get('/alRecipes/:id', (req, res) =>{
+    const id =parseInt(req.params.id);
+    
+        const chef_recipe = recipes.find(recipe =>recipe.recipe_id === id);
+        res.send(chef_recipe)
+})
 app.get('/chefs/', (req, res) =>{
     res.send(chefs)
 });
